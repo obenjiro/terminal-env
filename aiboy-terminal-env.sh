3957edee68df47756906652f63751114ff1021b7
Ai_boy <ai_boy@live.ru>
2015-01-06 14:44:24 +0300

    Update aiboy-terminal-env.sh

--- aiboy-terminal-env.sh
+++ aiboy-terminal-env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get -y install curl wget vim tmux git
+sudo apt-get -y install curl wget vim tmux git dconf-cli
 
 cd ~
 git init
@@ -9,3 +9,7 @@ git remote add origin https://github.com/aiboy/terminal-env.git
 git pull origin master
 git reset --hard HEAD
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
+gnome-terminal-colors-solarized/install.sh
