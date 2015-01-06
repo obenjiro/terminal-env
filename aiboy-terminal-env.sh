3e3dbf930148fc99db4270da46fd20bdcd645d95
Ai_boy <ai_boy@live.ru>
2015-01-06 14:35:24 +0300

    Update aiboy-terminal-env.sh

--- aiboy-terminal-env.sh
+++ aiboy-terminal-env.sh
@@ -6,6 +6,6 @@ sudo apt-get -y install curl wget vim tmux git
 cd ~
 git init
 git remote add origin https://github.com/aiboy/terminal-env.git
-git pull remote master
+git pull origin master
 git reset --hard HEAD
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
