0976dd9389a607bf233d1b998ad6ddfba3f7c00d
Ai_boy <ai_boy@live.ru>
2015-09-06 03:55:15 +0300

    update of color scheme setup

--- ubuntu-env.sh
+++ ubuntu-env.sh
@@ -33,8 +33,14 @@ killall tmux
 
 git config --global core.editor "vim"
 # solorized color cheme for ubuntu
-git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
-gnome-terminal-colors-solarized/set_dark.sh
+wget --no-check-certificate https://raw.github.com/seebi/dircolors-solarized/master/dircolors.ansi-dark
+mv dircolors.ansi-dark .dircolors
+eval `dircolors ~/.dircolors`
+
+git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+cd gnome-terminal-colors-solarized
+./set_dark.sh
+cd ~
 
 # reload shell - so nvm will appear immidiatly
 exec bash
