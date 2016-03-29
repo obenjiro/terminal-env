4e966bc3d1816c188ad13e0efcdb5749e2af9c90
Ai_boy <ai_boy@live.ru>
2016-03-29 19:49:32 +0400

    shell script for mac-os

---
+++ mac-os.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+brew install curl wget vim tmux git htop
+
+# getting config files and installing plugins
+cd ~
+rm mac-os.sh
+git init
+git remote add origin https://github.com/aiboy/terminal-env.git
+git pull origin master
+git reset --hard HEAD
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+# tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# will force to reload tmux configuration ( since we kill it )
+killall tmux
+
+# install tmux plugins
+~/.tmux/plugins/tpm/bin/install_plugins
+
+git config --global core.editor "vim"
